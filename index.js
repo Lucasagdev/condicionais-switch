2e6267821e0603e58a5f1714f9b0c76cc3e4f81c
Lucas Gomes <lucasagostini400@gmail.com>
2024-08-30 22:54:39 -0300

    Usando switch no javascript

---
+++ index.js
@@ -0,0 +1,41 @@
+// switch(paramentro){
+//  case valor 1: 
+//      <bloco de declaracao>
+//      break
+//  case valor 2: 
+//      <bloco de declaracao>
+//}
+
+// let nomeFilme = 'Vingadores'
+
+// switch(nomeFilme){
+//     case 'Vingadores':
+//         console.log('É o filme dos Vingadores')
+//         break
+//     case 'Batman vs Superman':
+//         console.log('É o filme do Batman vs Superman')
+//         break
+//     case 'Senhor dos Anéis':
+//         console.log('É o filme do Senhor dos Anéis')
+//         break
+//     default:
+//         console.log('É outro filme')
+// }
+
+let avaliacao = 5
+
+switch(avaliacao){
+    case 1:
+    case 2:
+        console.log('Filme ruim')
+        break
+    case 3:
+    case 4:
+        console.log('Filme mediano')
+        break
+    case 5:
+        console.log('Filme excelente')
+        break
+    default:
+        console.log('Nota inválida')
+}
